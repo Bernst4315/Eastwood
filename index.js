e41e9bf07ddca1bdad223a265ebd3e775e933fee
bernst4315 <bernst4315@gmail.com>
2025-01-03 11:55:27 -0700

    made a battle button

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-alert("After a humiliating defeat in the West Town tournament, you train in the Eastwood forest");
-alert("The Eastwood is known for it's monsters and people tend to avoid it");
+// alert("After a humiliating defeat in the West Town tournament at the hands of your rival, you train in the Eastwood forest");
+// alert("The Eastwood is known for it's monsters and people tend to avoid it");
+const battleBtn = document.querySelector("button");  
+const mainChar = {
+    name: "Adam",
+    level: 1,
+    hp: 10,
+    attack: 1,
+};
+
+const monster = {
+    name: "Creature",
+    level:1,
+    hp: 5,
+    attack: 1,
+}
+
+battleBtn.addEventListener("click", () => {
+    alert("you attacked");
+})
